28d9cca329610a09aaf4cb5fd3571f8090dfde9f
Gabriel Theodoro <gabrielth29@gmail.com>
2021-06-23 15:37:39 +0000

    changes made from theia lab environment

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = new express();
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -18,3 +19,116 @@ function getNLUInstance(){
 });
 return naturalLanguageUnderstanding;
 }
+
+
+app.use(express.static('client'))
+
+const cors_app = require('cors');
+app.use(cors_app());
+
+app.get("/",(req,res)=>{
+    res.render('index.html');
+  });
+
+app.get("/url/emotion", (req,res) => {
+
+   const analyzeParams = {
+ 'url': req.query.url,
+ 'features': {
+ 'entities': {
+ 'emotion': true,
+ 'limit': 1
+ }
+ }
+ }
+const naturalLanguageUnderstanding = getNLUInstance();
+ 
+ naturalLanguageUnderstanding.analyze(analyzeParams)
+ .then(analysisResults => {
+     console.log(analysisResults);
+ console.log(JSON.stringify(analysisResults.result.entities[0].emotion,null,2));
+ return res.send(analysisResults.result.entities[0].emotion,null,2);
+ //return res.send(analysisResults);
+ })
+ .catch(err => {
+ return res.send("Could not do desired operation "+err);
+ });
+ 
+});
+
+app.get("/url/sentiment", (req,res) => {
+      const analyzeParams = {
+ 'url': req.query.url,
+ 'features': {
+ 'entities': {
+ 'sentiment': true,
+ 'limit': 1
+ }
+ }
+ }
+ const naturalLanguageUnderstanding = getNLUInstance();
+ 
+ naturalLanguageUnderstanding.analyze(analyzeParams)
+ .then(analysisResults => {
+     console.log(analysisResults);
+ console.log(JSON.stringify(analysisResults.result.entities[0].sentiment,null,2));
+ return res.send(analysisResults.result.entities[0].sentiment.label,null,2);
+ //return res.send(analysisResults);
+ })
+ .catch(err => {
+ return res.send("Could not do desired operation "+err);
+ });
+});
+
+app.get("/text/emotion", (req,res) => {
+     const analyzeParams = {
+ 'text': req.query.text,
+ 'features': {
+ 'entities': {
+ 'emotion': true,
+ 'limit': 1
+ }
+ }
+ }
+ const naturalLanguageUnderstanding = getNLUInstance();
+ 
+ naturalLanguageUnderstanding.analyze(analyzeParams)
+ .then(analysisResults => {
+     console.log(analysisResults);
+ //console.log(JSON.stringify(analysisResults.result.entities[0].emotion,null,2));
+return res.send(analysisResults.result.entities[0].emotion,null,2);
+ //return res.send(analysisResults);
+ })
+ .catch(err => {
+ return res.send("Could not do desired operation "+err);
+ });
+});
+
+app.get("/text/sentiment", (req,res) => {
+    const analyzeParams = {
+ 'text': req.query.text,
+ 'features': {
+ 'entities': {
+ 'sentiment': true,
+ 'limit': 1
+ }
+ }
+ }
+  const naturalLanguageUnderstanding = getNLUInstance();
+ 
+ naturalLanguageUnderstanding.analyze(analyzeParams)
+ .then(analysisResults => {
+     console.log(analysisResults);
+ console.log(JSON.stringify(analysisResults.result.entities[0].sentiment,null,2));
+ return res.send(analysisResults.result.entities[0].sentiment.label,null,2);
+ //return res.send(analysisResults);
+ })
+ .catch(err => {
+ return res.send("Could not do desired operation "+err);
+ });
+});
+
+let server = app.listen(8080, () => {
+    console.log('Listening', server.address().port)
+})
+
